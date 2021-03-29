6a43e85e7e7d4bdfd21874412dea4b4d6fd0f6b2
Brian Crisp <bcrisp084@yahoo.com>
2021-03-28 21:54:27 -0400

    added server file for starting the app

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+//listen to the port
+app.listen(PORT,()=>{
+    console.log(`listen to the port ${PORT}`);
+});
